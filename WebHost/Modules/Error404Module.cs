7b2606a8d828c21e834c405aa4e87f8f75fb222f
Aleksey Vyaznikov <alexv47@mail.ru>
2017-10-19 10:05:12 +0300

    renew WebHost

---
+++ WebHost/Modules/Error404Module.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace WebHost.Modules
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class Error404Module
+    {
+        private readonly AppFunc next;
+
+        public Error404Module(AppFunc next)
+        {
+            this.next = next ?? throw new ArgumentNullException("next");
+        }
+
+        public Task Invoke(IDictionary<string, object> env)
+        {
+            var body = Encoding.UTF8.GetBytes("404");
+
+            var response = new OwinResponse(env)
+            {
+                ReasonPhrase = "",
+                StatusCode = 404,
+                ContentType = "text/plain;charset=utf-8",
+                ContentLength = body.Length
+            };
+
+            return response.WriteAsync(body);
+        }
+    }
+}
---
+++ WebHost/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+using WebHost.Modules;
+
+
+namespace WebHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            //Creation, configuration and installation of modules for the pipeline
+
+            //var config = new HttpConfiguration();
+            //config.Routes.MapHttpRoute("default", "{controller}");
+            //appBuilder.UseWebApi(config);
+
+            appBuilder.Use<Error404Module>();
+
+
+        }
+    }
+}
